988df484e63c2612030c09c6caf121fa5b1400b3
statox <me@statox.fr>
2023-11-29 22:14:07 +0100

    Create test route

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Route } from './types';
 import { route as getRemoteTime } from './getRemoteTime';
 import { route as checkChordsUrl } from './chordsCheckRoute';
+import { route as testRoute } from './testRoute';
 
-export const routes: Route[] = [getRemoteTime, checkChordsUrl];
+export const routes: Route[] = [getRemoteTime, checkChordsUrl, testRoute];
