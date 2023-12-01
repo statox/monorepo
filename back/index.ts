c32232dab99cdea400e65534071ecaa52de9b109
statox <me@statox.fr>
2023-12-01 18:35:22 +0100

    Enable cors

--- back/index.ts
+++ back/index.ts
@@ -1,9 +1,17 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
 import { routes } from './src/routes';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(
+    cors({
+        // TODO have a proper local setup to avoid localhost in prod
+        origin: ['https://statox.github.io', 'http://localhost:8080']
+    })
+);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
