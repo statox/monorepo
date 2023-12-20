62d3e12241a87cf83b7f7d32c8f44457e473c90e
statox <me@statox.fr>
2023-12-20 22:42:44 +0100

    Remove /test route

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -3,12 +3,10 @@ import { route as addChordLinkVisit } from './addChordLinkVisit';
 import { route as getChordLinksVisitCount } from './getChordLinksVisitCount';
 import { route as getRemoteTime } from './getRemoteTime';
 import { route as checkChordsUrl } from './chordsCheckRoute';
-import { route as testRoute } from './testRoute';
 
 export const routes: Route[] = [
     addChordLinkVisit,
     getChordLinksVisitCount,
     getRemoteTime,
-    checkChordsUrl,
-    testRoute
+    checkChordsUrl
 ];
