dcfc0b641764178dc8d485a9e0bcb25c832007fa
statox <me@statox.fr>
2024-01-09 22:32:57 +0100

    AddEntry: Stub s3Send to test PutObjectCommand parameters

--- back/tests/routes/clipboard/addEntry.test.ts
+++ back/tests/routes/clipboard/addEntry.test.ts
@@ -2,8 +2,15 @@ import request from 'supertest';
 import { expect } from 'chai';
 import { app } from '../../../src/app';
 import { mysqlCheckContains, mysqlFixture } from '../../helpers/mysql';
+import sinon from 'sinon';
+import { S3 } from '../../../src/services/s3';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
 
+let s3Send: sinon.SinonSpy;
 describe('clipboard/addEntry', () => {
+    beforeEach(() => (s3Send = sinon.spy(S3, 'send')));
+    afterEach(() => s3Send.restore());
+
     it('should not create duplicate entry', async () => {
         await mysqlFixture({
             Clipboard: [
@@ -108,6 +115,17 @@ describe('clipboard/addEntry', () => {
             .attach('file', buffer)
             .expect(200);
 
+        sinon.assert.calledOnce(s3Send);
+        sinon.assert.calledWithMatch(s3Send, sinon.match.instanceOf(PutObjectCommand));
+        sinon.assert.calledWithMatch(
+            s3Send,
+            sinon.match.has('input', sinon.match.has('Bucket', 'clipboard'))
+        );
+        sinon.assert.calledWithMatch(
+            s3Send,
+            sinon.match.has('input', sinon.match.has('ContentType', 'application/octet-stream'))
+        );
+
         await mysqlCheckContains({
             Clipboard: [
                 {
@@ -118,34 +136,51 @@ describe('clipboard/addEntry', () => {
         });
     });
 
-    it('When uploading a file should set an extension based on the file type', async () => {
-        await request(app)
-            .post('/clipboard/addEntry')
-            .set('content-type', 'multipart/form-data')
-            .field('name', 'image')
-            .field('content', 'image')
-            .attach('file', 'tests/assets/glider.png')
-            .expect(200);
+    describe('When uploading a file should set an extension based on the file type', () => {
+        it('- png', async () => {
+            await request(app)
+                .post('/clipboard/addEntry')
+                .set('content-type', 'multipart/form-data')
+                .field('name', 'image')
+                .field('content', 'image')
+                .attach('file', 'tests/assets/glider.png')
+                .expect(200);
 
-        await request(app)
-            .post('/clipboard/addEntry')
-            .set('content-type', 'multipart/form-data')
-            .field('name', 'animated_image')
-            .field('content', 'animated_image')
-            .attach('file', 'tests/assets/burns.gif')
-            .expect(200);
+            sinon.assert.calledWithMatch(
+                s3Send,
+                sinon.match.has('input', sinon.match.has('ContentType', 'image/png'))
+            );
+            await mysqlCheckContains({
+                Clipboard: [
+                    {
+                        name: 'image',
+                        s3Key: (value: string) => value.match(/.*image.png/) !== null
+                    }
+                ]
+            });
+        });
 
-        await mysqlCheckContains({
-            Clipboard: [
-                {
-                    name: 'image',
-                    s3Key: (value: string) => value.match(/.*image.png/) !== null
-                },
-                {
-                    name: 'animated_image',
-                    s3Key: (value: string) => value.match(/.*animated_image.gif/) !== null
-                }
-            ]
+        it('- gif', async () => {
+            await request(app)
+                .post('/clipboard/addEntry')
+                .set('content-type', 'multipart/form-data')
+                .field('name', 'animated_image')
+                .field('content', 'animated_image')
+                .attach('file', 'tests/assets/burns.gif')
+                .expect(200);
+
+            sinon.assert.calledWithMatch(
+                s3Send,
+                sinon.match.has('input', sinon.match.has('ContentType', 'image/gif'))
+            );
+            await mysqlCheckContains({
+                Clipboard: [
+                    {
+                        name: 'animated_image',
+                        s3Key: (value: string) => value.match(/.*animated_image.gif/) !== null
+                    }
+                ]
+            });
         });
     });
 });
