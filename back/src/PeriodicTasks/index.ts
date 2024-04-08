6b10a4fed00b33e0408c8bb748b3ec8289d03f22
statox <me@statox.fr>
2024-04-08 21:13:32 +0200

    PeriodicTask: Disable CheckCPF

--- back/src/PeriodicTasks/index.ts
+++ back/src/PeriodicTasks/index.ts
@@ -1,10 +1,11 @@
 import { logMessageToSlack } from '../services/logging/slack';
 
 export const startPeriodicTasks = () => {
-    checkCPF();
-    setInterval(checkCPF, 1000 * 60 * 10); // 10 minutes;
+    // checkCPF();
+    // setInterval(checkCPF, 1000 * 60 * 10); // 10 minutes;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const checkCPF = async () => {
     const URL = 'https://www.moncompteformation.gouv.fr/espace-prive';
     logMessageToSlack(`Checking ${URL}`);
