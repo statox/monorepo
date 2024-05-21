81e6ee9aa9a07e69bf7265791836fce052834ad7
statox <me@statox.fr>
2024-05-21 13:52:47 +0200

    Variabilize kimsufi watcher

--- back/src/PeriodicTasks/kimsufiWatcher.ts
+++ back/src/PeriodicTasks/kimsufiWatcher.ts
@@ -2,13 +2,21 @@ import jsdom from 'jsdom';
 import { slog } from '../services/logging';
 const { JSDOM } = jsdom;
 
+const URL = 'https://eco.ovhcloud.com/fr/?display=list&range=kimsufi';
+const CSS_SELECTOR =
+    '.ods-all-servers > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > details:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div:nth-child(12) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2) > div:nth-child(5) > div:nth-child(1)';
+const CURRENT_CONTENT = '  Prochainement disponible';
+const WATCHER_NAME = 'Kimsufi watcher';
+const WATCHER_MESSAGE =
+    'Go buy a KS-4! https://eco.ovhcloud.com/fr/?display=list&range=kimsufi&prices=10%7C20';
+const LOG_EVERY_X_CHECKS = 12;
+
 let checksDoneSinceLastLog = -1;
 
 export const watchKimsufi = async () => {
-    const selector =
-        '.ods-all-servers > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > details:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div:nth-child(12) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2) > div:nth-child(5) > div:nth-child(1)';
+    const selector = CSS_SELECTOR;
 
-    const page = await fetch('https://eco.ovhcloud.com/fr/?display=list&range=kimsufi');
+    const page = await fetch(URL);
     const text = await page.text();
 
     const dom = new JSDOM(text);
@@ -18,19 +26,18 @@ export const watchKimsufi = async () => {
     const childElementText = childElement?.textContent ?? 'N/A';
     const contentClean = childElementText.replaceAll('\n', '');
 
-    if (contentClean !== '  Prochainement disponible') {
-        slog.log({ logToSlack: true, message: 'Kimsufi watcher', status: contentClean });
+    if (contentClean !== CURRENT_CONTENT) {
+        slog.log({ logToSlack: true, message: WATCHER_NAME, status: contentClean });
         slog.log({
             logToSlack: true,
-            message:
-                'Go buy a KS-4! https://eco.ovhcloud.com/fr/?display=list&range=kimsufi&prices=10%7C20'
+            message: WATCHER_MESSAGE
         });
         return;
     }
 
     checksDoneSinceLastLog++;
-    if (checksDoneSinceLastLog === 0 || checksDoneSinceLastLog >= 12) {
-        slog.log({ message: 'Kimsufi watcher', status: contentClean, checksDoneSinceLastLog });
+    if (checksDoneSinceLastLog === 0 || checksDoneSinceLastLog >= LOG_EVERY_X_CHECKS) {
+        slog.log({ message: WATCHER_NAME, status: contentClean, checksDoneSinceLastLog });
         checksDoneSinceLastLog = 0;
     }
 };
