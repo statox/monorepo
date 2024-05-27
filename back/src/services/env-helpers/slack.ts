40c9794cb571a47405d9222483083f36a688686e
statox <me@statox.fr>
2024-05-27 19:11:24 +0200

    Slack - Update environment variables name

--- back/src/services/env-helpers/slack.ts
+++ back/src/services/env-helpers/slack.ts
@@ -1,8 +1,8 @@
 import { isProd } from './env';
 
 // My user ID to be @mentioned on slack (Found in the UI)
-export const SLACK_USERID = isProd ? process.env.LOGS_SLACK_USERID! : 'AZERTY';
-export const SLACK_WEBHOOK_URL = isProd ? process.env.LOGS_SLACK_WEBHOOK_URL! : 'http://127.0.0.1';
+export const SLACK_USERID = isProd ? process.env.SLACK_USERID! : 'AZERTY';
+export const SLACK_WEBHOOK_URL = isProd ? process.env.SLACK_WEBHOOK_URL! : 'http://127.0.0.1';
 
 if (!SLACK_USERID || !SLACK_WEBHOOK_URL) {
     throw new Error('Missing slack config env variable');
