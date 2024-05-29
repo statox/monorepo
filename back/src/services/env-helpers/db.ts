1a7b1b404c42ce420da995ad4fb2f637b89fe519
statox <me@statox.fr>
2024-05-29 12:45:01 +0200

    Docker env - Rework compose to avoid conflict with other projects

--- back/src/services/env-helpers/db.ts
+++ back/src/services/env-helpers/db.ts
@@ -5,8 +5,8 @@ import { slog } from '../logging';
 import { ConfigError } from './errors';
 
 const PROD_URL = process.env.JAWSDB_URL!;
-const DEV_URL = 'mysql://root:example@127.0.0.1:13306/db';
-const TESTS_URL = 'mysql://root:example@127.0.0.1:13306/tests';
+const DEV_URL = 'mysql://root:example@127.0.0.1:23306/db';
+const TESTS_URL = 'mysql://root:example@127.0.0.1:23306/tests';
 
 let dbUrl: string;
 if (isProd) {
