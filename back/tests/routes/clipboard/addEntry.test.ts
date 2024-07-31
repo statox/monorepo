ba81dc35cc44bdceaf7a889c5aaba7817c1e4f48
statox <me@statox.fr>
2024-07-31 20:34:13 +0200

    Clipboard - Reorganize addEntry tests

--- back/tests/routes/clipboard/addEntry.test.ts
+++ back/tests/routes/clipboard/addEntry.test.ts
@@ -5,196 +5,220 @@ import { mysqlCheckContains, mysqlFixture } from '../../helpers/mysql';
 import { s3CheckCall } from '../../helpers/s3';
 
 describe('clipboard/addEntry', () => {
-    it('should reject a query if no file and no content are provided', async () => {
-        await request(app)
-            .post('/clipboard/addEntry')
-            .set('Accept', 'application/json')
-            .send({ name: 'A cool entry' })
-            .expect(400)
-            .then((response) => {
-                expect(response.text).to.equal('FILE_OR_CONTENT_REQUIRED');
-            });
-
-        // Allow file without a comment
-        const buffer = Buffer.from('some data');
-        await request(app)
-            .post('/clipboard/addEntry')
-            .set('content-type', 'multipart/form-data')
-            .field('name', 'entry name')
-            .attach('file', buffer)
-            .expect(200);
-    });
-
-    it('should not create duplicate entry', async () => {
-        await mysqlFixture({
-            Clipboard: [
-                {
-                    id: 1,
-                    name: 'A cool entry',
-                    content: 'Look at that content',
-                    creationDateUnix: 10,
-                    ttl: 100,
-                    linkId: 'aabbccdd'
-                }
-            ]
+    describe('should reject', () => {
+        it('Query with no file and no content', async () => {
+            await request(app)
+                .post('/clipboard/addEntry')
+                .set('Accept', 'application/json')
+                .send({ name: 'A cool entry' })
+                .expect(400)
+                .then((response) => {
+                    expect(response.text).to.equal('FILE_OR_CONTENT_REQUIRED');
+                });
         });
 
-        await request(app)
-            .post('/clipboard/addEntry')
-            .set('Accept', 'application/json')
-            .send({
-                name: 'A cool entry',
-                content: 'Foo'
-            })
-            .expect(400)
-            .then((response) => {
-                expect(response.text).to.equal('ER_DUP_ENTRY');
-            });
-
-        await mysqlCheckContains({
-            Clipboard: [
-                {
-                    name: 'A cool entry',
-                    content: 'Look at that content',
-                    creationDateUnix: 10
-                }
-            ]
-        });
-
-        s3CheckCall({ nbCalls: 0 });
-    });
-
-    it('should not call s3 when avoiding creating a duplicate entry', async () => {
-        const buffer = Buffer.from('some data');
-        await mysqlFixture({
-            Clipboard: [
-                {
-                    id: 1,
-                    name: 'A cool entry',
-                    content: 'Look at that content',
-                    creationDateUnix: 10,
-                    ttl: 100,
-                    linkId: 'aabbccdd'
-                }
-            ]
-        });
-
-        await request(app)
-            .post('/clipboard/addEntry')
-            .set('content-type', 'multipart/form-data')
-            .field('name', 'A cool entry')
-            .field('content', 'entry content')
-            .attach('file', buffer)
-            .expect(400)
-            .then((response) => {
-                expect(response.text).to.equal('ER_DUP_ENTRY');
-            });
-
-        await mysqlCheckContains({
-            Clipboard: [
-                {
-                    name: 'A cool entry',
-                    content: 'Look at that content',
-                    creationDateUnix: 10
-                }
-            ]
-        });
-
-        s3CheckCall({ nbCalls: 0 });
-    });
-
-    it('should create new private entry with default ttl to 5 minutes', async () => {
-        await request(app)
-            .post('/clipboard/addEntry')
-            .set('Accept', 'application/json')
-            .send({
-                name: 'A cool entry',
-                content: 'Look at that content'
-            })
-            .expect(200);
-
-        await mysqlCheckContains({
-            Clipboard: [
-                {
-                    name: 'A cool entry',
-                    content: 'Look at that content',
-                    ttl: 60 * 5,
-                    isPublic: 0,
-                    s3Key: null,
-                    creationDateUnix: {
-                        aroundTimestamp: 'NOW()',
-                        precision: '1 SECOND'
+        it('Query with duplicate entry (no file)', async () => {
+            await mysqlFixture({
+                Clipboard: [
+                    {
+                        id: 1,
+                        name: 'A cool entry',
+                        content: 'Look at that content',
+                        creationDateUnix: 10,
+                        ttl: 100,
+                        linkId: 'aabbccdd'
                     }
-                }
-            ]
+                ]
+            });
+
+            await request(app)
+                .post('/clipboard/addEntry')
+                .set('Accept', 'application/json')
+                .send({
+                    name: 'A cool entry',
+                    content: 'Foo'
+                })
+                .expect(400)
+                .then((response) => {
+                    expect(response.text).to.equal('ER_DUP_ENTRY');
+                });
+
+            await mysqlCheckContains({
+                Clipboard: [
+                    {
+                        name: 'A cool entry',
+                        content: 'Look at that content',
+                        creationDateUnix: 10
+                    }
+                ]
+            });
+
+            s3CheckCall({ nbCalls: 0 });
         });
 
-        s3CheckCall({ nbCalls: 0 });
+        it('Query with duplicate entry (file) - should not call S3', async () => {
+            const buffer = Buffer.from('some data');
+            await mysqlFixture({
+                Clipboard: [
+                    {
+                        id: 1,
+                        name: 'A cool entry',
+                        content: 'Look at that content',
+                        creationDateUnix: 10,
+                        ttl: 100,
+                        linkId: 'aabbccdd'
+                    }
+                ]
+            });
+
+            await request(app)
+                .post('/clipboard/addEntry')
+                .set('content-type', 'multipart/form-data')
+                .field('name', 'A cool entry')
+                .field('content', 'entry content')
+                .attach('file', buffer)
+                .expect(400)
+                .then((response) => {
+                    expect(response.text).to.equal('ER_DUP_ENTRY');
+                });
+
+            await mysqlCheckContains({
+                Clipboard: [
+                    {
+                        name: 'A cool entry',
+                        content: 'Look at that content',
+                        creationDateUnix: 10
+                    }
+                ]
+            });
+
+            s3CheckCall({ nbCalls: 0 });
+        });
     });
 
-    it('should create new public entry with custom ttl', async () => {
-        await request(app)
-            .post('/clipboard/addEntry')
-            .set('Accept', 'application/json')
-            .send({
-                name: 'A cool entry',
-                content: 'Look at that content',
-                ttlSeconds: 60,
-                isPublic: true
-            })
-            .expect(200);
+    describe('should susccesfully create a new entry', () => {
+        it('private entry - default ttl is set to 5 minutes', async () => {
+            await request(app)
+                .post('/clipboard/addEntry')
+                .set('Accept', 'application/json')
+                .send({
+                    name: 'A cool entry',
+                    content: 'Look at that content'
+                })
+                .expect(200);
 
-        await mysqlCheckContains({
-            Clipboard: [
-                {
+            await mysqlCheckContains({
+                Clipboard: [
+                    {
+                        name: 'A cool entry',
+                        content: 'Look at that content',
+                        ttl: 60 * 5,
+                        isPublic: 0,
+                        s3Key: null,
+                        creationDateUnix: {
+                            aroundTimestamp: 'NOW()',
+                            precision: '1 SECOND'
+                        }
+                    }
+                ]
+            });
+
+            s3CheckCall({ nbCalls: 0 });
+        });
+
+        it('public entry - with custom ttl', async () => {
+            await request(app)
+                .post('/clipboard/addEntry')
+                .set('Accept', 'application/json')
+                .send({
                     name: 'A cool entry',
                     content: 'Look at that content',
-                    ttl: 60,
-                    isPublic: 1,
-                    s3Key: null,
-                    creationDateUnix: {
-                        aroundTimestamp: 'NOW()',
-                        precision: '1 SECOND'
+                    ttlSeconds: 60,
+                    isPublic: true
+                })
+                .expect(200);
+
+            await mysqlCheckContains({
+                Clipboard: [
+                    {
+                        name: 'A cool entry',
+                        content: 'Look at that content',
+                        ttl: 60,
+                        isPublic: 1,
+                        s3Key: null,
+                        creationDateUnix: {
+                            aroundTimestamp: 'NOW()',
+                            precision: '1 SECOND'
+                        }
                     }
-                }
-            ]
+                ]
+            });
+
+            s3CheckCall({ nbCalls: 0 });
         });
 
-        s3CheckCall({ nbCalls: 0 });
+        it('file entry - file is uploaded to S3', async () => {
+            const buffer = Buffer.from('some data');
+
+            await request(app)
+                .post('/clipboard/addEntry')
+                .set('content-type', 'multipart/form-data')
+                .field('name', 'entry name')
+                .attach('file', buffer)
+                .expect(200);
+
+            s3CheckCall({ nbCalls: 1 });
+            s3CheckCall({
+                commandType: 'PutObject',
+                input: {
+                    Bucket: 'clipboard',
+                    ContentType: 'application/octet-stream'
+                }
+            });
+
+            await mysqlCheckContains({
+                Clipboard: [
+                    {
+                        name: 'entry name',
+                        s3Key: (value: string) => value.match(/.*entry name/) !== null
+                    }
+                ]
+            });
+        });
+
+        it('file entry (no additional content) - file is uploaded to S3', async () => {
+            const buffer = Buffer.from('some data');
+
+            await request(app)
+                .post('/clipboard/addEntry')
+                .set('content-type', 'multipart/form-data')
+                .field('name', 'entry name')
+                .field('content', 'entry content')
+                .attach('file', buffer)
+                .expect(200);
+
+            s3CheckCall({ nbCalls: 1 });
+            s3CheckCall({
+                commandType: 'PutObject',
+                input: {
+                    Bucket: 'clipboard',
+                    ContentType: 'application/octet-stream'
+                }
+            });
+
+            await mysqlCheckContains({
+                Clipboard: [
+                    {
+                        name: 'entry name',
+                        s3Key: (value: string) => value.match(/.*entry name/) !== null
+                    }
+                ]
+            });
+        });
     });
 
-    it('When a file is provided should create new entry and upload the file to R2', async () => {
-        const buffer = Buffer.from('some data');
-
-        await request(app)
-            .post('/clipboard/addEntry')
-            .set('content-type', 'multipart/form-data')
-            .field('name', 'entry name')
-            .field('content', 'entry content')
-            .attach('file', buffer)
-            .expect(200);
-
-        s3CheckCall({ nbCalls: 1 });
-        s3CheckCall({
-            commandType: 'PutObject',
-            input: {
-                Bucket: 'clipboard',
-                ContentType: 'application/octet-stream'
-            }
-        });
-
-        await mysqlCheckContains({
-            Clipboard: [
-                {
-                    name: 'entry name',
-                    s3Key: (value: string) => value.match(/.*entry name/) !== null
-                }
-            ]
-        });
-    });
-
-    describe('When uploading a file should set an extension based on the file type', () => {
-        it('- png', async () => {
+    describe('should set an extension based on the file type', () => {
+        it('png', async () => {
             await request(app)
                 .post('/clipboard/addEntry')
                 .set('content-type', 'multipart/form-data')
@@ -219,7 +243,7 @@ describe('clipboard/addEntry', () => {
             });
         });
 
-        it('- gif', async () => {
+        it('gif', async () => {
             await request(app)
                 .post('/clipboard/addEntry')
                 .set('content-type', 'multipart/form-data')
