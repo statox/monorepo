e9f4e13269366603b438d8c10be33035dc456eee
statox <me@statox.fr>
2024-08-21 00:47:34 +0200

    Tests framework - Speed up mysqlClearTable fixture step by running the tables in parallel

--- back/tests/helpers/mysql/mysqlClearTables.ts
+++ back/tests/helpers/mysql/mysqlClearTables.ts
@@ -1,15 +1,19 @@
 import { RowDataPacket } from 'mysql2';
 import { db } from '../../../src/libs/databases/db';
 
+let allTables: string[];
 const listAllTables = async () => {
+    if (allTables) {
+        return allTables;
+    }
     const [rows] = await db.query<RowDataPacket[]>('SHOW TABLES');
     const tables = rows.map((row) => row.Tables_in_tests);
+    allTables = tables;
     return tables;
 };
 
 export const mysqlClearAllTables = async () => {
     const tables = await listAllTables();
-    for (const table of tables) {
-        await db.query(`TRUNCATE TABLE ${table};`);
-    }
+    const promises = tables.map((table) => db.query(`TRUNCATE TABLE ${table};`));
+    await Promise.all(promises);
 };
