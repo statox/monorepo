d387b4fbcfcf19cd0262fd10988e867011a8c0e7
statox <me@statox.fr>
2024-08-30 16:37:30 +0200

    ESlint - Upgrade to eslint v9

---
+++ back/eslint.config.mjs
@@ -0,0 +1,44 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [
+    ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'),
+    {
+        plugins: {
+            '@typescript-eslint': typescriptEslint
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.node
+            },
+
+            parser: tsParser,
+            ecmaVersion: 2020,
+            sourceType: 'module'
+        },
+
+        rules: {
+            // Don't check if the error of try...catch blocks are used
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    caughtErrors: 'none'
+                }
+            ]
+        }
+    }
+];
