693c2443c7828bfbcdcbcd6abc97e1bb5efea494
statox <me@statox.fr>
2025-07-28 00:33:42 +0200

    dump-db - Minor tweaks

--- back/src/tools/new_db_helpers/dump-db.sh
+++ back/src/tools/new_db_helpers/dump-db.sh
@@ -2,7 +2,7 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-BACKUP_FILE='./backup.gz'
+BACKUP_FILE="./backup_$(date +%y_%m_%d-%H_%M).gz"
 TMP_BACKUP_FILE='./backup_tmp.sql'
 
 source "$SCRIPTPATH/set-db-creds.sh" "$1"
@@ -15,4 +15,4 @@ echo "Got creds. Dumping DB"
 docker exec apistatox.mysql /usr/bin/mysqldump -h "$host" -u "$user" --password="$password" --no-tablespaces "$db" >  "$TMP_BACKUP_FILE"
 
 gzip -c "$TMP_BACKUP_FILE" > "$BACKUP_FILE"
-# rm "$TMP_BACKUP_FILE"
+rm "$TMP_BACKUP_FILE"
