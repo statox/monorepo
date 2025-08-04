14699a5d261c265e4c04ade97ec71e5fb211cbf4
statox <me@statox.fr>
2025-08-04 19:52:06 +0200

    DB - Stop using deprecated url

--- back/src/libs/databases/db.ts
+++ back/src/libs/databases/db.ts
@@ -1,18 +1,18 @@
 import mysql, { Pool, PoolOptions } from 'mysql2/promise';
-import url from 'url';
+import { URL } from 'url';
 import { slog } from '../modules/logging/index.js';
 import { config } from '../../packages/config/index.js';
 
 export let db: Pool;
 export const initDb = async () => {
     slog.log('app', 'init db');
-    const parsedUrl = url.parse(config.mysql.connectionUrl);
+    const parsedUrl = new URL(config.mysql.connectionUrl);
 
     if (!parsedUrl) {
         throw new Error('Couldnt parse DB url');
     }
 
-    const requiredFields: (keyof url.UrlWithStringQuery)[] = ['hostname', 'port', 'auth', 'path'];
+    const requiredFields: (keyof URL)[] = ['hostname', 'port', 'username', 'password', 'pathname'];
     for (const field of requiredFields) {
         if (!parsedUrl[field]) {
             throw new Error(`Missing ${field} in DB url`);
@@ -22,9 +22,9 @@ export const initDb = async () => {
     const connectionOptions: PoolOptions = {
         host: parsedUrl.hostname!,
         port: Number(parsedUrl.port!),
-        user: parsedUrl.auth!.split(':')[0],
-        password: parsedUrl.auth!.split(':')[1],
-        database: parsedUrl.path!.slice(1),
+        user: parsedUrl.username,
+        password: parsedUrl.password,
+        database: parsedUrl.pathname.replace(/^\//, ''),
         waitForConnections: true,
         connectionLimit: 5,
         queueLimit: 1000
