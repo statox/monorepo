66e907e35c423d4593d852020b39600ac1f6abc8
statox <me@statox.fr>
2025-08-06 00:11:03 +0200

    Garden - Create Garden app and Barnsley fern page

---
+++ front/src/routes/(garden)/garden/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import '$lib/styles/new.css';
+    import '$lib/styles/new_theme.css';
+    import '$lib/styles/new_override.css';
+    import { onMount, type Snippet } from 'svelte';
+
+    interface Props {
+        children?: Snippet;
+    }
+    let { children }: Props = $props();
+
+    onMount(() => {
+        // For now always set dark mode.
+        // TODO maybe add ThemeSwitcherNavItem component later
+        const htmlElement = document.documentElement;
+        htmlElement.dataset.theme = 'dark';
+    });
+</script>
+
+{@render children?.()}
---
+++ front/src/routes/(garden)/garden/+page.svelte
@@ -0,0 +1,1 @@
+<a href="garden/barnsley">Barnsley fern</a>
