3d36969dae6ca24a4f86a92d27d86192240d8680
statox <me@statox.fr>
2025-09-21 00:21:56 +0200

    Gravitrip - 2players - Simplify page logic and add winning cells

--- front/src/routes/(apps)/gravitrip/2players/+page.svelte
+++ front/src/routes/(apps)/gravitrip/2players/+page.svelte
@@ -5,13 +5,19 @@
     import { PUBLIC_API_URL } from '$env/static/public';
 
     let socket: WebSocket;
-    let connected = $state(false);
-    let gameReady = $state(false);
     let youAre: number | null = $state(null);
     let board: Board | null = $state(null);
     let boardState: BoardState | null = $state(null);
-    let gameOver = $state(false);
     let gameOverReason: string | null = $state('');
+    let winningCells: number[][] | null = $state(null);
+
+    type PageState =
+        | 'connecting'
+        | 'waiting_for_opponent'
+        | 'playing'
+        | 'game_over'
+        | 'disconnected';
+    let pageState: PageState = $state('connecting');
 
     // Note that we replace only "http" even for the "https" prod endpoint
     // because in prod we want to use wss://
@@ -29,32 +35,32 @@
 
     const restart = () => {
         socket = new WebSocket(WS_SERVER_URL);
+        pageState = 'connecting';
         board = null;
+        boardState = null;
+        winningCells = null;
+        gameOverReason = null;
 
         socket.onopen = () => {
-            connected = true;
-            gameOver = false;
-            gameOverReason = null;
+            pageState = 'waiting_for_opponent';
         };
 
         socket.onclose = (event: CloseEvent) => {
-            connected = false;
-            gameOver = true;
-            gameReady = false;
+            pageState = 'game_over';
             gameOverReason = event.reason;
         };
 
         socket.onmessage = (event) => {
             const data = JSON.parse(event.data);
             if (data.type === 'game_ready') {
-                gameReady = true;
+                pageState = 'playing';
                 youAre = data.youAre;
                 board = data.board;
             } else if (data.type === 'update_board') {
                 board = data.board;
                 boardState = data.boardState;
+                winningCells = data.winningCells;
             } else if (data.type === 'waiting_for_opponent') {
-                gameReady = false;
                 boardState = null;
             }
         };
@@ -69,25 +75,39 @@
 
 <h3>Simple WebSocket Client</h3>
 
-{#if gameOver}
-    <p>Game over - {gameOverReason}</p>
-    <button onclick={restart}>New game</button>
-{:else if !connected}
-    <p>Connecting…</p>
-{:else if !gameReady}
-    <p>Waiting for another player…</p>
-{:else}
-    <p>Game started! You are Player {youAre}</p>
+{#if pageState === 'connecting'}
+    <p>Connecting to the server…</p>
 {/if}
 
-<span style="font-weight: bold">
-    {#if boardState === BoardState.draw}
-        Game Over
-    {:else if boardState === BoardState.winPlayer1 || boardState === BoardState.winPlayer2}
-        Player {boardState} wins
-    {/if}
-</span>
+{#if pageState === 'waiting_for_opponent'}
+    <p>Waiting for an opponent to connect…</p>
+{/if}
 
-{#if board !== null}
-    <BoardComp nbColumns={7} nbRows={6} {board} winningCells={null} onMove={tryMove} />
+{#if pageState === 'playing'}
+    {#if board !== null}
+        <p>You are Player {youAre}</p>
+        <BoardComp nbColumns={7} nbRows={6} {board} {winningCells} onMove={tryMove} />
+    {:else}
+        <p>Something went wrong. Board is not defined.</p>
+    {/if}
+{/if}
+
+{#if pageState === 'game_over'}
+    <p>Game over - {gameOverReason}</p>
+    <button onclick={restart}>New game</button>
+    <span style="font-weight: bold">
+        {#if boardState === BoardState.draw}
+            Game Over
+        {:else if boardState === BoardState.winPlayer1 || boardState === BoardState.winPlayer2}
+            Player {boardState} wins
+        {/if}
+    </span>
+    {#if board !== null}
+        <BoardComp nbColumns={7} nbRows={6} {board} {winningCells} onMove={tryMove} />
+    {/if}
+{/if}
+
+{#if pageState === 'disconnected'}
+    <p>Disconnected</p>
+    <button onclick={restart}>New game</button>
 {/if}
