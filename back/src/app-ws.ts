603dc334e39bb7a44f00b1bbf8140be3d36a880e
statox <me@statox.fr>
2025-10-03 00:25:10 +0200

    WS - Attach request ID to incoming websockets

--- back/src/app-ws.ts
+++ back/src/app-ws.ts
@@ -2,23 +2,30 @@ import { WebSocket, WebSocketServer } from 'ws';
 import { IncomingMessage } from 'node:http';
 import { slog } from './libs/modules/logging/index.js';
 import { routesWS } from './libs/routes/index.js';
+import { randomUUID } from 'node:crypto';
+import { config } from './packages/config/index.js';
 
 export const initWsServer = (wss: WebSocketServer) => {
     slog.log('app', 'Init WebSocket server');
     wss.on('connection', routeWebsocket);
 };
 
-const routeWebsocket = (ws: WebSocket, req: IncomingMessage) => {
-    const [path, params] = (req.url || '').split('?');
-    slog.log('ws', 'access-log-ws', { url: req.url });
+export class EnrichedWebSocket extends WebSocket {
+    uuid?: string;
+}
 
+const routeWebsocket = (ws: EnrichedWebSocket, req: IncomingMessage) => {
+    ws.uuid = config.env.isTests ? '00000000-0000-0000-0000-000000000001' : randomUUID();
+    slog.log('ws', 'access-log-ws', { url: req.url, requestId: ws.uuid });
+
+    const [path, params] = (req.url || '').split('?');
     for (const route of routesWS.list) {
         if (path === route.path) {
             return route.onConnection(ws, params);
         }
     }
 
-    slog.log('ws', 'Rejected websocket with invalid path', { url: req.url });
+    slog.log('ws', 'Rejected websocket with invalid path', { url: req.url, requestId: ws.uuid });
     ws.send(JSON.stringify({ error: 'invalid_path' }));
     ws.close();
 };
--- back/tests/framework/websocket.test.ts
+++ back/tests/framework/websocket.test.ts
@@ -6,6 +6,17 @@ import { wss } from '../../src/app.js';
 const WS_SERVER_URL = 'ws://localhost:3001';
 
 describe('WebSocket endpoint routing', async () => {
+    it('should create a log on new connection', async () => {
+        const client = new TestWebSocket(WS_SERVER_URL + '/foobar');
+        await client.waitUntil('open');
+
+        await client.waitForMessage(JSON.stringify({ error: 'invalid_path' }));
+        th.slog.checkLog('ws', 'access-log-ws', {
+            url: '/foobar',
+            requestId: '00000000-0000-0000-0000-000000000001'
+        });
+    });
+
     it('should reject a non existing route with a 404 code', async () => {
         const client = new TestWebSocket(WS_SERVER_URL + '/foobar');
         await client.waitUntil('open');
