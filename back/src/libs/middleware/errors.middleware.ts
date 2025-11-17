a33c5c8a7d1930781a92cb3dc968990863f671e4
statox <me@statox.fr>
2025-11-17 17:12:52 +0100

    Error middleware - Rework logs and logic

--- back/src/libs/middleware/errors.middleware.ts
+++ back/src/libs/middleware/errors.middleware.ts
@@ -16,22 +16,31 @@ import {
     Auth_InvalidScopeError,
     Auth_UnauthorizedError
 } from '../modules/auth/index.js';
+import { slog } from '../modules/logging/slog.js';
 
 export const errorHandler = async (
     error: Error,
-    _request: Request,
+    request: Request,
     response: Response,
     next: NextFunction
 ) => {
     response.locals.loggableContext?.addData('error', error);
-    slackNotifier.notifySlack({ error, directMention: true });
+    // This slog shouldn't be needed since we are adding error to the loggable context
+    // but it doesn't seem to work properly
+    slog.log('app', 'Error caught by middleware', { error, url: request.url });
+    slackNotifier.notifySlack({
+        error,
+        message: `Error on url ${request.url}`
+    });
+
+    let status = 500;
+    let message = 'Internal Server Error';
 
     if (error instanceof OutputValidationError) {
         response.status(500).json({ message: 'Failed output validation' });
-        return next();
-    }
-
-    if (
+        status = 500;
+        message = 'Failed output validation';
+    } else if (
         error instanceof ItemAlreadyExistsError ||
         error instanceof FileOrContentRequiredError ||
         error instanceof EntryAlreadyExistsError ||
@@ -40,31 +49,23 @@ export const errorHandler = async (
         error instanceof RecipeNotFoundError ||
         error instanceof SensorDoesNotExistError
     ) {
-        response.status(400).json({ message: error.message });
-        return next();
-    }
-
-    if (
+        status = 400;
+        message = error.message;
+    } else if (
         error instanceof Auth_UnauthorizedError ||
         error instanceof Auth_ForbiddenForUserError ||
         error instanceof Auth_InvalidScopeError
     ) {
-        response.status(401).json({ message: error.message });
-        return next();
+        status = 401;
+        message = error.message;
+    } else if (error instanceof ApiKeyError) {
+        status = error.status;
+        message = error.message;
+    } else if (error instanceof ValidationError) {
+        status = 400;
+        message = JSON.stringify(error.validationErrors);
     }
 
-    if (error instanceof ApiKeyError) {
-        response.status(error.status).json({ message: error.message });
-        return next();
-    }
-
-    if (error instanceof ValidationError) {
-        response.status(400).send(error.validationErrors);
-        return next();
-    }
-
-    const status = 500;
-    const message = 'Internal Server Error';
     response.status(status).json({ message });
     next();
 };
