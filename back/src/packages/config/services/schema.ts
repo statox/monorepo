857cf282799ba7e8055b99129308278cb29a4650
statox <me@statox.fr>
2026-02-09 16:04:23 +0100

    dotenvx - Remove dummy variable now that dotenvx is setup completely

--- back/src/packages/config/services/schema.ts
+++ back/src/packages/config/services/schema.ts
@@ -13,10 +13,6 @@ export const configSchema = {
         'timeouts'
     ],
     properties: {
-        dummy: {
-            type: 'string',
-            description: 'This is a dummy env variable to test dotenvx. TO BE REMOVED'
-        },
         mysql: {
             type: 'object',
             additionalProperties: false,
