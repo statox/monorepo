b35200afe1a7907a17c725aa06edf21c66e880d9
statox <me@statox.fr>
2026-02-23 20:22:40 +0000

    Deploying to gh-pages from @ statox/monorepo@7e7ac3ea21fba42627247d62a75e556175fd6a82 🚀

---
+++ _app/immutable/chunks/CP4hlzJG.js
@@ -0,0 +1,1 @@
+import{c as o}from"./DjG0_Xs-.js";const s=o.cookbook.addRecipe,c=o.cookbook.listRecipes,t=o.cookbook.listIngredients,i=o.cookbook.getRecipe;export{t as a,s as b,i as g,c as l};
--- _app/immutable/entry/start.DpGJ7B3l.js
+++
@@ -1,1 +0,0 @@
-import{l as o,c as r}from"../chunks/DtZQ2_jm.js";export{o as load_css,r as start};
