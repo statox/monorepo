ece66454a4fb8b6e4aafa8172cd97227914005b9
statox <me@statox.fr>
2026-04-26 18:42:37 +0200

    feat(yt-helper): add route group layout

---
+++ front/src/routes/(yt-helper)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import '$lib/styles/new.css';
+    import '$lib/styles/new_theme.css';
+    import '$lib/styles/new_override.css';
+    import type { Snippet } from 'svelte';
+
+    interface Props {
+        children?: Snippet;
+    }
+
+    let { children }: Props = $props();
+</script>
+
+{@render children?.()}
