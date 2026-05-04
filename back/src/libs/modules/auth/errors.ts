7718472ca7ea344b0e3d99e0a1d7ebaf1c13c5b2
statox <me@statox.fr>
2026-05-04 16:17:51 +0200

    refactor(errors): migrate Auth_* errors to AppError

--- back/src/libs/modules/auth/errors.ts
+++ back/src/libs/modules/auth/errors.ts
@@ -1,26 +1,28 @@
+import { AppError } from '../../errors/AppError.js';
+
 /**
  * User was likely logged out
  */
-export class Auth_UnauthorizedError extends Error {
+export class Auth_UnauthorizedError extends AppError {
     constructor() {
-        super('UNAUTHORIZED');
+        super({ code: 'UNAUTHORIZED', httpStatus: 401 });
     }
 }
 
 /**
  * User doesn't have the right to access the resource
  */
-export class Auth_ForbiddenForUserError extends Error {
+export class Auth_ForbiddenForUserError extends AppError {
     constructor() {
-        super('FORBIDDEN_FOR_USER');
+        super({ code: 'FORBIDDEN_FOR_USER', httpStatus: 401 });
     }
 }
 
 /**
  * There is a scope configuration issue
  */
-export class Auth_InvalidScopeError extends Error {
+export class Auth_InvalidScopeError extends AppError {
     constructor() {
-        super('INVALID_SCOPE');
+        super({ code: 'INVALID_SCOPE', httpStatus: 401 });
     }
 }
